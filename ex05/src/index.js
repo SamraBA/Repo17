913f1538a25b132d86cdca6c5b1911db7e1ec994
SamraBA <bandicsamra@gmail.com>
2021-09-30 08:02:01 +0200

    destructuring arrays & pluralsight page

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+   
+let foo = 12, boo = 45;
+
+function main(foo, boo) {
+
+  // Only change code below this line
+
+  [foo, boo] = [boo, foo];
+
+  // Only change code above this line
+  
+  return { foo, boo };
+}
+
+console.log(main(foo, boo));
+module.exports = main;
